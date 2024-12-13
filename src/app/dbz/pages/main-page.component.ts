0188d6e5f97a97f2d6134fec94ac3770697bdd09
AinhoaFrancoIglesias <ainhoa.figlesias@gmail.com>
2024-12-13 12:11:34 +0100

    Creacion dbz.service.ts y cambios de mainpage ts y html

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,5 @@
-import { Character } from './../interfaces/character.interface';
 import { Component} from '@angular/core';
+import { DbzService } from '../services/dbz.service';
 
 
 @Component({
@@ -8,32 +8,7 @@ import { Component} from '@angular/core';
 })
 
 export class MainPageComponent {
-
-  // crea un array de tipo character para meterle personajes
-  public characters: Character[] = [{
-    name: 'Krillin',
-    power: 1000
-  },{
-    name:'Goku',
-    power: 9500
-  },{
-    name:"Vegeta",
-    power: 7500
-  }];
-
- // onDelete = Index value: number;
-
-  // usa el metodo push para añadir el personaje nuevo al array
-  onNewCharacter(character:Character): void{ // recibe un objeto de tipo character (cuando es invocado, se le pasa por parametros el nmbre y el poder del personaje, al invocar el metodo)
-    this.characters.push(character);
+  constructor( public dbzService: DbzService) {
+    //inyectamos la dependdencia
   }
-
-  // borra un personaje de la lista basandose en el indice del mismo
-  onDeleteCharacter(index:number){
-    //this.characters.splice(1,2) //esto está comentado pero lo que hace es borrar el segunod y tercer heroe (lo explico justo abajo):
-    /*//elimina un elemento del array desde una posicion concreta. (index, 1) significa que va a borrar desde el indice 1 posicion nada mas.
-     si fuera index, 2 borraria 2 personajes partiendo desde el que se ha pulsado el boton X */
-    this.characters.splice(index,1); 
-  }
-
 }
---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Character } from './../interfaces/character.interface';
+
+
+@Injectable({providedIn: 'root'})
+export class DbzService {
+    // crea un array de tipo character para meterle personajes
+      public characters: Character[] = [{
+        name: 'Krillin',
+        power: 1000
+      },{
+        name:'Goku',
+        power: 9500
+      },{
+        name:"Vegeta",
+        power: 7500
+      }];
+    
+     // onDelete = Index value: number;
+    
+      // usa el metodo push para añadir el personaje nuevo al array
+      onNewCharacter(character:Character): void{ // recibe un objeto de tipo character (cuando es invocado, se le pasa por parametros el nmbre y el poder del personaje, al invocar el metodo)
+        this.characters.push(character);
+      }
+    
+      // borra un personaje de la lista basandose en el indice del mismo
+      onDeleteCharacter(index:number){
+        //this.characters.splice(1,2) //esto está comentado pero lo que hace es borrar el segunod y tercer heroe (lo explico justo abajo):
+        /*//elimina un elemento del array desde una posicion concreta. (index, 1) significa que va a borrar desde el indice 1 posicion nada mas.
+         si fuera index, 2 borraria 2 personajes partiendo desde el que se ha pulsado el boton X */
+        this.characters.splice(index,1); 
+      }
+    
+}
